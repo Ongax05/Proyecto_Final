9e3b2730f37f2749386487aa380622ed7dda67ad
Jhosep <jhoseppulido1129@gmail.com>
2023-11-23 15:53:24 -0500

    feat: :sparkles: Generic Utilities added

---
+++ Application/Repositories/GenericRepository.cs
@@ -0,0 +1,68 @@
+using System.Linq.Expressions;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistency.Data;
+
+namespace Application.Repository;
+
+public class GenericIntRepository<T> : IGenericInt<T>
+    where T : BaseEntity
+{
+    private readonly ApiContext _context;
+
+    public GenericIntRepository(ApiContext context)
+    {
+        _context = context;
+    }
+
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+
+    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public virtual async Task<T> GetByIdAsync(string id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public virtual void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public virtual void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+
+    public virtual async Task<int> HowMany()
+    {
+        return await _context.Set<T>().CountAsync();
+    }
+}
---
+++ Application/Repositories/GenericStringRepository.cs
@@ -0,0 +1,63 @@
+using System.Linq.Expressions;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistency.Data;
+
+namespace Application.Repository;
+
+public class GenericStringRepository<T> : IGenericString<T>
+    where T : StringBaseEntity
+{
+    private readonly ApiContext _context;
+
+    public GenericStringRepository(ApiContext context)
+    {
+        _context = context;
+    }
+
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public virtual void AddRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().AddRange(entities);
+    }
+
+    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return _context.Set<T>().Where(expression);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(string id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public virtual void Remove(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+
+    public virtual void RemoveRange(IEnumerable<T> entities)
+    {
+        _context.Set<T>().RemoveRange(entities);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+
+    public virtual async Task<int> HowMany()
+    {
+        return await _context.Set<T>().CountAsync();
+    }
+}
